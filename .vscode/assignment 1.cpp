6e7b7c2c75ea499df2d10ebc0c29ceff9f9720e3
Marcus Dela Cruz <delacruzmarcus098@gmail.com>
2026-05-25 02:48:27 -0400

    Commit for Week 01 programming assignment

--- .vscode/assignment 1.cpp
+++ .vscode/assignment 1.cpp
@@ -5,24 +5,24 @@ int main() {
 
     //defining the variables to use in ending output
 
-    std::string tcgName;
+    std::string tcg;
     std::string favoriteCard;
-    int yearsCollecting;
+    int TimeCollected;
 
     //using getline to take the string of the respone in case its longer then one word
     std::cout << "What Trading Card Game do you collect? ";
-    std::getline(std::cin, tcgName);
+    std::getline(std::cin, tcg);
 
     std::cout << "What is your favorite card from that Trading Card Game? ";
     std::getline(std::cin, favoriteCard);
 
     std::cout << "How many years have you been collecting this Trading Card Game? ";
-    std::cin >> yearsCollecting;
+    std::cin >> TimeCollected;
 
 
     //Take all the input values to create the short paragraph
 
-    std::cout << "\nYou have been collecting " << tcgName << " for " << yearsCollecting << " years with " << favoriteCard << " being your favorite card after all that time!";
+    std::cout << "\nYou have been collecting " << tcg << " for " << TimeCollected << " years with " << favoriteCard << " being your favorite card after all that time!";
 
     return 0;
 }
